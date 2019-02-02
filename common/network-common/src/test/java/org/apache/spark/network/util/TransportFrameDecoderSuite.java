01372cec7208c9a82be64f8a92d0a1515c1ce560
Liupengcheng <liupengcheng@xiaomi.com>
2019-02-02 15:28:36 +0800

    Update three plan for consolidation and their tests report

--- common/network-common/src/test/java/org/apache/spark/network/util/TransportFrameDecoderSuite.java
+++ common/network-common/src/test/java/org/apache/spark/network/util/TransportFrameDecoderSuite.java
@@ -93,42 +93,320 @@ public class TransportFrameDecoderSuite {
     System.out.println(String.format("%s %s on %s %s", vmName, runtimeVersion, osName, osVersion));
   }
 
-  private CompositeByteBuf createCompositeBuf(ByteBufAllocator alloc, int numComponents, int size, int writtenBytes) {
+  // Processor info
+  // cat /proc/cpuinfo|grep "model name"
+
+  private class Metrics {
+    long totalTime;
+  }
+  private CompositeByteBuf createCompositeBuf(
+      ByteBufAllocator alloc, int numComponents, int size, int writtenBytes, Metrics metrics, int plan) {
+    long lastConsolidateWriterIndex = 0;
     CompositeByteBuf compositeByteBuf = alloc.compositeBuffer(Integer.MAX_VALUE);
     for (int i =0; i< numComponents; i++) {
       ByteBuf buf = alloc.ioBuffer(size);
-      buf.writerIndex(writtenBytes);
+      buf.writeBytes(ByteBuffer.allocate(writtenBytes));
       compositeByteBuf.addComponent(buf).writerIndex(compositeByteBuf.writerIndex() + buf.readableBytes());
+      if (plan == 2) {
+        // plan 2
+        if (i % 16 == 0) {
+          long start = System.currentTimeMillis();
+          compositeByteBuf.consolidate();
+          long cost = System.currentTimeMillis() - start;
+          metrics.totalTime += cost;
+        }
+      }
+      if (plan == 3) {
+        // plan 3
+        if (compositeByteBuf.writerIndex() - lastConsolidateWriterIndex >= 300 * 1024 * 1024) {
+          long start = System.currentTimeMillis();
+          compositeByteBuf.consolidate();
+          long cost = System.currentTimeMillis() - start;
+          metrics.totalTime += cost;
+          lastConsolidateWriterIndex = compositeByteBuf.writerIndex();
+        }
+      }
     }
+    if (plan == 1) {
+      // plan 1
+      long start = System.currentTimeMillis();
+      compositeByteBuf.consolidate();
+      long cost = System.currentTimeMillis() - start;
+      metrics.totalTime += cost;
+    }
+
     return compositeByteBuf;
   }
 
+  // 1. only consolidate the final compositebuf
+  // 2. consolidate the compositebuf once the maxComponents is exceeded, default to 16
+  // 3. consolidate the compositebuf when the accumulated written bytes exceeds 0.3 * memoryOverhead( 300M for 1GB)
+  // NOTE: assume sixty percent of memoryOverhead can be used in shuffle(when in shuffle phase, the memoryOverhead are mainly used here),
+  // in the worst case, the writerIndex is equal to the capacity, then we must reserve half of this memory for consolidation, thus we get 0.3.
+  // this is a conservative esitmate, for most cases, we can make the threshold higher, but we just keep it as unchanged for better safety.
   private void testConsolidateWithLoop(String testName, ByteBufAllocator alloc, int numComponents, int size, int util, int loopCount) {
-    long totalTime = 0L;
     int writtenBytes = (int)((double)size * util / 100);
+    Metrics metrics = new Metrics();
     for (int i = 0; i < loopCount; i++) {
-      CompositeByteBuf buf = createCompositeBuf(alloc, numComponents, size, writtenBytes);
-      long start = System.currentTimeMillis();
-      buf.consolidate();
-      long cost = System.currentTimeMillis() - start;
-      totalTime += cost;
+      // Test consolidate with different plan - 1 for plan1, 2 for plan2, 3 for plan3
+      CompositeByteBuf buf = createCompositeBuf(alloc, numComponents, size, writtenBytes, metrics, 2);
       buf.release();
     }
     System.out.println("[" + testName + "]");
     System.out.println("Allocating " + writtenBytes + " bytes");
-    System.out.println("Time cost with " + loopCount + " loop for consolidating: " + totalTime + " millis");
+    System.out.println("Time cost with " + loopCount + " loop for consolidating: " + metrics.totalTime + " millis");
     System.out.println();
   }
+  ///////////////////////////////////////////////////////////
+  //  Java HotSpot(TM) 64-Bit Server VM 1.8.0_111-b14 on Linux 4.15.0-43-generic
+  //  Intel(R) Core(TM) i7-4790 CPU @ 3.60GHz
+
+  //  --- Test Reports for plan 1 ------
+  //  [test consolidate 1000 buffers each with 1m, 50% used for 1 loop]
+  //  Allocating 524288 bytes
+  //  Time cost with 1 loop for consolidating: 207 millis
+  //
+  //  [test consolidate 1000 buffers each with 1m, 100% used for 1 loop]
+  //  Allocating 1048576 bytes
+  //  Time cost with 1 loop for consolidating: 340 millis
+  //
+  //  [test consolidate 1000 buffers each with 1m, 50% used for 10 loop]
+  //  Allocating 524288 bytes
+  //  Time cost with 10 loop for consolidating: 1635 millis
+  //
+  //  [test consolidate 1000 buffers each with 1m, 100% used for 10 loop]
+  //  Allocating 1048576 bytes
+  //  Time cost with 10 loop for consolidating: 3172 millis
+  //
+  //  [test consolidate 1000 buffers each with 1m, 50% used for 50 loop]
+  //  Allocating 524288 bytes
+  //  Time cost with 50 loop for consolidating: 8017 millis
+  //
+  //  [test consolidate 1000 buffers each with 1m, 100% used for 50 loop]
+  //  Allocating 1048576 bytes
+  //  Time cost with 50 loop for consolidating: 15497 millis
+
+  //  [test consolidate 100 buffers each with 10m, 50% used for 1 loop]
+  //  Allocating 5242880 bytes
+  //  Time cost with 1 loop for consolidating: 223 millis
+  //
+  //  [test consolidate 100 buffers each with 10m, 100% used for 1 loop]
+  //  Allocating 10485760 bytes
+  //  Time cost with 1 loop for consolidating: 451 millis
+  //
+  //  [test consolidate 100 buffers each with 10m, 50% used for 10 loop]
+  //  Allocating 5242880 bytes
+  //  Time cost with 10 loop for consolidating: 1924 millis
+  //
+  //  [test consolidate 100 buffers each with 10m, 100% used for 10 loop]
+  //  Allocating 10485760 bytes
+  //  Time cost with 10 loop for consolidating: 3787 millis
+  //
+  //  [test consolidate 100 buffers each with 10m, 50% used for 50 loop]
+  //  Allocating 5242880 bytes
+  //  Time cost with 50 loop for consolidating: 8870 millis
+  //
+  //  [test consolidate 100 buffers each with 10m, 100% used for 50 loop]
+  //  Allocating 10485760 bytes
+  //  Time cost with 50 loop for consolidating: 17472 millis
+  //
+  //  [test consolidate 20 buffers each with 50m, 50% used for 1 loop]
+  //  Allocating 26214400 bytes
+  //  Time cost with 1 loop for consolidating: 184 millis
+  //
+  //  [test consolidate 20 buffers each with 50m, 100% used for 1 loop]
+  //  Allocating 52428800 bytes
+  //  Time cost with 1 loop for consolidating: 367 millis
+  //
+  //  [test consolidate 20 buffers each with 50m, 50% used for 10 loop]
+  //  Allocating 26214400 bytes
+  //  Time cost with 10 loop for consolidating: 1847 millis
+  //
+  //  [test consolidate 20 buffers each with 50m, 100% used for 10 loop]
+  //  Allocating 52428800 bytes
+  //  Time cost with 10 loop for consolidating: 3638 millis
+  //
+  //  [test consolidate 20 buffers each with 50m, 50% used for 50 loop]
+  //  Allocating 26214400 bytes
+  //  Time cost with 50 loop for consolidating: 9126 millis
+  //
+  //  [test consolidate 20 buffers each with 50m, 100% used for 50 loop]
+  //  Allocating 52428800 bytes
+  //  Time cost with 50 loop for consolidating: 19391 millis
+  //
+  //  [test consolidate 10 buffers each with 100m, 50% used for 1 loop]
+  //  Allocating 52428800 bytes
+  //  Time cost with 1 loop for consolidating: 211 millis
+  //
+  //  [test consolidate 10 buffers each with 100m, 100% used for 1 loop]
+  //  Allocating 104857600 bytes
+  //  Time cost with 1 loop for consolidating: 400 millis
+  //
+  //  [test consolidate 10 buffers each with 100m, 50% used for 10 loop]
+  //  Allocating 52428800 bytes
+  //  Time cost with 10 loop for consolidating: 1954 millis
+  //
+  //  [test consolidate 10 buffers each with 100m, 100% used for 10 loop]
+  //  Allocating 104857600 bytes
+  //  Time cost with 10 loop for consolidating: 3846 millis
+  //
+  //  [test consolidate 10 buffers each with 100m, 50% used for 50 loop]
+  //  Allocating 52428800 bytes
+  //  Time cost with 50 loop for consolidating: 9747 millis
+  //
+  //  [test consolidate 10 buffers each with 100m, 100% used for 50 loop]
+  //  Allocating 104857600 bytes
+  //  Time cost with 50 loop for consolidating: 19542 millis
+
+  //  --- Test Reports for plan 2 ------
+  //
+  // [test consolidate 1000 buffers each with 1m, 50% used for 1 loop]
+  // Allocating 524288 bytes
+  // Time cost with 1 loop for consolidating: 5338 millis
+  //
+  // [test consolidate 1000 buffers each with 1m, 100% used for 1 loop]
+  // Allocating 1048576 bytes
+  // Time cost with 1 loop for consolidating: 10220 millis
+  //
+  // [test consolidate 1000 buffers each with 1m, 50% used for 10 loop]
+  // Allocating 524288 bytes
+  // Time cost with 10 loop for consolidating: 49249 millis
+  //
+  // [test consolidate 1000 buffers each with 1m, 100% used for 10 loop]
+  // Allocating 1048576 bytes
+  // Time cost with 10 loop for consolidating: 99247 millis
+  //
+  // [test consolidate 1000 buffers each with 1m, 50% used for 50 loop]
+  // Allocating 524288 bytes
+  // Time cost with 50 loop for consolidating: 249160 millis
+  // ...... too slow
+  //
+  //  --- Test Reports for plan 3 ---
+  //  [test consolidate 1000 buffers each with 1m, 50% used for 1 loop]
+  //  Allocating 524288 bytes
+  //  Time cost with 1 loop for consolidating: 116 millis
+  //
+  //  [test consolidate 1000 buffers each with 1m, 100% used for 1 loop]
+  //  Allocating 1048576 bytes
+  //  Time cost with 1 loop for consolidating: 635 millis
+  //
+  //  [test consolidate 1000 buffers each with 1m, 50% used for 10 loop]
+  //  Allocating 524288 bytes
+  //  Time cost with 10 loop for consolidating: 1003 millis
+  //
+  //  [test consolidate 1000 buffers each with 1m, 100% used for 10 loop]
+  //  Allocating 1048576 bytes
+  //  Time cost with 10 loop for consolidating: 5702 millis
+  //
+  //  [test consolidate 1000 buffers each with 1m, 50% used for 50 loop]
+  //  Allocating 524288 bytes
+  //  Time cost with 50 loop for consolidating: 4799 millis
+  //
+  //  [test consolidate 1000 buffers each with 1m, 100% used for 50 loop]
+  //  Allocating 1048576 bytes
+  //  Time cost with 50 loop for consolidating: 28440 millis
+  //
+  //  [test consolidate 100 buffers each with 10m, 50% used for 1 loop]
+  //  Allocating 5242880 bytes
+  //  Time cost with 1 loop for consolidating: 96 millis
+  //
+  //  [test consolidate 100 buffers each with 10m, 100% used for 1 loop]
+  //  Allocating 10485760 bytes
+  //  Time cost with 1 loop for consolidating: 571 millis
+  //
+  //  [test consolidate 100 buffers each with 10m, 50% used for 10 loop]
+  //  Allocating 5242880 bytes
+  //  Time cost with 10 loop for consolidating: 940 millis
+  //
+  //  [test consolidate 100 buffers each with 10m, 100% used for 10 loop]
+  //  Allocating 10485760 bytes
+  //  Time cost with 10 loop for consolidating: 5727 millis
+  //
+  //  [test consolidate 100 buffers each with 10m, 50% used for 50 loop]
+  //  Allocating 5242880 bytes
+  //  Time cost with 50 loop for consolidating: 4739 millis
+  //
+  //  [test consolidate 100 buffers each with 10m, 100% used for 50 loop]
+  //  Allocating 10485760 bytes
+  //  Time cost with 50 loop for consolidating: 28356 millis
+  //
+  //  [test consolidate 20 buffers each with 50m, 50% used for 1 loop]
+  //  Allocating 26214400 bytes
+  //  Time cost with 1 loop for consolidating: 96 millis
+  //
+  //  [test consolidate 20 buffers each with 50m, 100% used for 1 loop]
+  //  Allocating 52428800 bytes
+  //  Time cost with 1 loop for consolidating: 577 millis
+  //
+  //  [test consolidate 20 buffers each with 50m, 50% used for 10 loop]
+  //  Allocating 26214400 bytes
+  //  Time cost with 10 loop for consolidating: 966 millis
+  //
+  //  [test consolidate 20 buffers each with 50m, 100% used for 10 loop]
+  //  Allocating 52428800 bytes
+  //  Time cost with 10 loop for consolidating: 5731 millis
+  //
+  //  [test consolidate 20 buffers each with 50m, 50% used for 50 loop]
+  //  Allocating 26214400 bytes
+  //  Time cost with 50 loop for consolidating: 4826 millis
+  //
+  //  [test consolidate 20 buffers each with 50m, 100% used for 50 loop]
+  //  Allocating 52428800 bytes
+  //  Time cost with 50 loop for consolidating: 28734 millis
+  //
+  //  [test consolidate 10 buffers each with 100m, 50% used for 1 loop]
+  //  Allocating 52428800 bytes
+  //  Time cost with 1 loop for consolidating: 98 millis
+  //
+  //  [test consolidate 10 buffers each with 100m, 100% used for 1 loop]
+  //  Allocating 104857600 bytes
+  //  Time cost with 1 loop for consolidating: 576 millis
+  //
+  //  [test consolidate 10 buffers each with 100m, 50% used for 10 loop]
+  //  Allocating 52428800 bytes
+  //  Time cost with 10 loop for consolidating: 965 millis
+  //
+  //  [test consolidate 10 buffers each with 100m, 100% used for 10 loop]
+  //  Allocating 104857600 bytes
+  //  Time cost with 10 loop for consolidating: 6122 millis
+  //
+  //  [test consolidate 10 buffers each with 100m, 50% used for 50 loop]
+  //  Allocating 52428800 bytes
+  //  Time cost with 50 loop for consolidating: 5228 millis
+  //
+  //  [test consolidate 10 buffers each with 100m, 100% used for 50 loop]
+  //  Allocating 104857600 bytes
+  //  Time cost with 50 loop for consolidating: 30797 millis
+  ////////////////////////////////////////////////////////////
 
   @Test
   public void benchmarkForConsolidation() throws Exception {
     PooledByteBufAllocator alloc = new PooledByteBufAllocator(true);
+
+    testConsolidateWithLoop("test consolidate 1000 buffers each with 1m, 50% used for 1 loop",
+        alloc, 1000, 1024 * 1024, 50, 1);
+
+    testConsolidateWithLoop("test consolidate 1000 buffers each with 1m, 100% used for 1 loop",
+        alloc, 1000, 1024 * 1024, 100, 1);
+    testConsolidateWithLoop("test consolidate 1000 buffers each with 1m, 50% used for 10 loop",
+        alloc, 1000, 1024 * 1024, 50, 10);
+
+    testConsolidateWithLoop("test consolidate 1000 buffers each with 1m, 100% used for 10 loop",
+        alloc, 1000, 1024 * 1024, 100, 10);
+
+    testConsolidateWithLoop("test consolidate 1000 buffers each with 1m, 50% used for 50 loop",
+        alloc, 1000, 1024 * 1024, 50, 50);
+
+    testConsolidateWithLoop("test consolidate 1000 buffers each with 1m, 100% used for 50 loop",
+        alloc, 1000, 1024 * 1024, 100, 50);
+    /////////
 
     testConsolidateWithLoop("test consolidate 100 buffers each with 10m, 50% used for 1 loop",
         alloc, 100, 1024 * 1024 * 10, 50, 1);
 
     testConsolidateWithLoop("test consolidate 100 buffers each with 10m, 100% used for 1 loop",
         alloc, 100, 1024 * 1024 * 10, 100, 1);
+
 
     testConsolidateWithLoop("test consolidate 100 buffers each with 10m, 50% used for 10 loop",
         alloc, 100, 1024 * 1024 * 10, 50, 10);
